de46b299458d912fdfa6f70b7680f22b9e307535
Tung No <111445323+tungno@users.noreply.github.com>
2024-08-15 09:44:03 +0200

    change main

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -18,8 +18,8 @@ var db *sql.DB
 
 func initDB() {
 	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/mmfood"
-	//dsn := "root:tungno@tcp(db:3306)/mmfood"
+	//dsn := "root:@tcp(127.0.0.1:3306)/mmfood"
+	dsn := "root:tungno@tcp(db:3306)/mmfood"
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
