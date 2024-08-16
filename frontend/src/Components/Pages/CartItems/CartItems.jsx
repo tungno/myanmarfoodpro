c8547822e1ee66c1e235b06a44561b18314263d7
Tung No <111445323+tungno@users.noreply.github.com>
2024-08-16 17:17:10 +0200

    translation fix

--- frontend/src/Components/Pages/CartItems/CartItems.jsx
+++ frontend/src/Components/Pages/CartItems/CartItems.jsx
@@ -1,20 +1,22 @@
 import React, { useContext } from 'react';
 import './CartItems.css';
+import { useTranslation } from "react-i18next";
 import { ShopContext } from "../../../Context/ShopContext";
 import remove_icon from '../../Asset/cart_cross_icon.png';
 
 const CartItems = () => {
+    const { t } = useTranslation();
     const { all_product, cartItems, removeFromCart, getTotalCartAmount } = useContext(ShopContext);
 
     return (
         <div className='cartitems'>
             <div className="cartitems-format-main">
-                <p>Products</p>
-                <p>Title</p>
-                <p>Price</p>
-                <p>Quantity</p>
-                <p>Total</p>
-                <p>Remove</p>
+                <p>{t('products')}</p>
+                <p>{t('title')}</p>
+                <p>{t('price')}</p>
+                <p>{t('quantity')}</p>
+                <p>{t('total')}</p>
+                <p>{t('remove')}</p>
             </div>
             <hr />
             {all_product.map((e) => {
@@ -31,7 +33,7 @@ const CartItems = () => {
                                     className='cartitems-remove-icon'
                                     src={remove_icon}
                                     onClick={() => removeFromCart(e.id)}
-                                    alt="Remove item"
+                                    alt={t('remove_item')}
                                 />
                             </div>
                             <hr />
@@ -42,30 +44,30 @@ const CartItems = () => {
             })}
             <div className="cartitems-down">
                 <div className="cartitems-total">
-                    <h1>Cart Totals</h1>
+                    <h1>{t('cart_totals')}</h1>
                     <div>
                         <div className="cartitems-total-item">
-                            <p>Subtotal</p>
+                            <p>{t('subtotal')}</p>
                             <p>{getTotalCartAmount()} NOK</p>
                         </div>
                         <hr />
                         <div className="cartitems-total-item">
-                            <p>Shipping Fee</p>
-                            <p>Free</p>
+                            <p>{t('shipping_fee')}</p>
+                            <p>{t('free')}</p>
                         </div>
                         <hr />
                         <div className="cartitems-total-item">
-                            <h3>Total</h3>
+                            <h3>{t('total')}</h3>
                             <h3>{getTotalCartAmount()} NOK</h3>
                         </div>
                     </div>
-                    <button>PROCEED TO CHECKOUT</button>
+                    <button>{t('proceed_to_checkout')}</button>
                 </div>
                 <div className="cartitems-promocode">
-                    <p>If you have a promo code, Enter it here</p>
+                    <p>{t('promo_code_text')}</p>
                     <div className="cartitems-promobox">
-                        <input type="text" placeholder='promo code' />
-                        <button>Submit</button>
+                        <input type="text" placeholder={t('promo_code_placeholder')} />
+                        <button>{t('submit')}</button>
                     </div>
                 </div>
             </div>
